10b6f057569ad085228e2244f86b67ad0ee27257
shuizhao.gh <shuizhao.gh@alibaba-inc.com>
2024-11-27 22:06:13 +0800

    test: refine test, add non-utf8 benchmark

--- test/log_parser_benchmark_test.py
+++ test/log_parser_benchmark_test.py
@@ -16,34 +16,26 @@ with open(os.path.join(workdir, 'flat.json'), 'r') as f:
     json_str = f.read()
 
 
-def lz4_logs_to_flat_json():
-    log_parser.lz4_logs_to_flat_json(compressed, raw_size, False, True)
+def test_lz4_logs_to_flat_json_utf8(benchmark):
+    benchmark(log_parser.lz4_logs_to_flat_json, compressed, raw_size, False, True)
 
 
-def test_lz4_logs_to_flat_json(benchmark):
-    result = benchmark(lz4_logs_to_flat_json)
-
-
-def logs_to_flat_json_str():
-    log_parser.logs_to_flat_json_str(rawdata)
+def test_lz4_logs_to_flat_json_b_value(benchmark):
+    benchmark(log_parser.lz4_logs_to_flat_json, compressed, raw_size, False, False)
 
 
 def test_logs_to_flat_json_str(benchmark):
-    result = benchmark(logs_to_flat_json_str)
-
-
-def lz4_logs_to_flat_json_str():
-    log_parser.lz4_logs_to_flat_json_str(compressed, raw_size)
+    benchmark(log_parser.logs_to_flat_json_str, rawdata)
 
 
 def test_lz4_logs_to_flat_json_str(benchmark):
-    result = benchmark(lz4_logs_to_flat_json_str)
+    benchmark(log_parser.lz4_logs_to_flat_json_str, compressed, raw_size)
 
 
-def multi_threaded_task(num_threads, func):
+def multi_threaded_task(num_threads, func, *args):
     threads = []
     for _ in range(num_threads):
-        thread = threading.Thread(target=func)
+        thread = threading.Thread(target=func, args=args)
         threads.append(thread)
         thread.start()
 
@@ -52,4 +44,4 @@ def multi_threaded_task(num_threads, func):
 
 
 def test_lz4_logs_to_flat_json_n_threads():
-    multi_threaded_task(4, lz4_logs_to_flat_json)
+    multi_threaded_task(4, log_parser.lz4_logs_to_flat_json, compressed, raw_size, False, True)
